bff0d11c9674795a0f1c241a05a98bf046aa1632
Nick <k.n.nuankaew@gmail.com>
2021-09-01 14:26:32 +0700

    add menu mobile

--- src/@core/layouts/components/app-navbar/AppNavbarVerticalLayout.vue
+++ src/@core/layouts/components/app-navbar/AppNavbarVerticalLayout.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="navbar-container d-flex content align-items-center">
     <!-- Nav Menu Toggler -->
-    <!-- <ul class="nav navbar-nav d-xl-none">
+    <ul class="nav navbar-nav d-xl-none">
       <li class="nav-item">
         <b-link class="nav-link" @click="toggleVerticalMenuActive">
           <feather-icon icon="MenuIcon" size="21" />
         </b-link>
       </li>
-    </ul> -->
+    </ul>
 
     <!-- Left Col -->
     <!-- <div
